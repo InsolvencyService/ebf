d712dbf93c20e02b753f4f908150e679d72853cc
Floyd Price <f.price@floydprice.com>
2015-09-17 16:26:25 +0100

    auto focus on the search box

--- modules/admin/client/search/search.js
+++ modules/admin/client/search/search.js
@@ -3,15 +3,18 @@ Template.admin_search.onCreated(function() {
   var urn = Session.set('urn_query', '');
     self.subscribe('admin_ebfs');
 });
+Template.admin_search.onRendered(function() {
+  $("#urn").focus();
+});
 
 Template.admin_search.helpers({
   ebfs: function() {
     var urn = Session.get('urn_query');
     return EBFs.find({sourceType: 'BKT', sourceId: {$regex: urn+".*", $options: 'i'}}, {sort: {createdAt: -1}, limit: 15  });
   },
-  ebfsOutstandingActions: function() {
-    return EBFs.find({sourceType: 'BKT', hasActions: true}, {sort: {createdAt: 1}, limit: 150  });
-  },
+  // ebfsOutstandingActions: function() {
+  //   return EBFs.find({sourceType: 'BKT', hasActions: true}, {sort: {createdAt: 1}, limit: 150  });
+  // },
   searched: function() {
     var urn = Session.get('urn_query');
     return urn !== '';

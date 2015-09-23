13e825f1b2cceb402b2c0a52530335d792a5eb17
Floyd Price <f.price@floydprice.com>
2015-09-23 13:47:12 +0100

    Added document type promt prior to upload (Notice or Document)

--- modules/admin/client/details/details.js
+++ modules/admin/client/details/details.js
@@ -1,8 +1,10 @@
 Template.admin_ebf_details.onCreated(function() {
   var self = this;
-  self.subscribe("admin_ebf", FlowRouter.current().params.urn, {onReady: function() {
-    self.subscribe("ebf_files",  EBFs.findOne()._id);
-  }});
+  self.subscribe("admin_ebf", FlowRouter.current().params.urn, {
+    onReady: function() {
+      self.subscribe("ebf_files", EBFs.findOne()._id);
+    }
+  });
 })
 
 Template.admin_ebf_details.helpers({
@@ -11,23 +13,57 @@ Template.admin_ebf_details.helpers({
   },
   ebf_files: function() {
     return EbfFiles.find();
+  },
+  isNotice: function() {
+    return this.documentType == "notice";
   }
 });
 
 
 Template.admin_ebf_details.events({
   "click #upload-btn": function() {
-    $("#upload-field").click();
+    showDocumentTypePrompt();
+
   },
   'change #upload-field': FS.EventHandlers.insertFiles(EbfFiles, {
-     metadata: function (fileObj) {
-       return {
-         ebf: EBFs.findOne()._id,
-         urn: FlowRouter.current().params.urn
-       };
-     },
-     after: function (error, fileObj) {
-       console.log("Inserted", fileObj.name);
-     }
-   })
+    metadata: function(fileObj) {
+      return {
+        ebf: EBFs.findOne()._id,
+        urn: FlowRouter.current().params.urn,
+        documentType: currentDocumentType
+      };
+    },
+    before: function() {
+
+    },
+    after: function(error, fileObj) {
+      console.log("Inserted", fileObj.name);
+    }
+  })
 });
+var currentDocumentType = "document";
+
+var showDocumentTypePrompt = function() {
+  bootbox.dialog({
+    message: "What type of document is this?",
+    title: "Specify a document type",
+    buttons: {
+      success: {
+        label: "Notice",
+        className: "btn-success",
+        callback: function() {
+          currentDocumentType = "notice";
+          $("#upload-field").click();
+        }
+      },
+      danger: {
+        label: "Document",
+        className: "btn-primary",
+        callback: function() {
+          currentDocumentType = "document";
+          $("#upload-field").click();
+        }
+      }
+    }
+  });
+}
